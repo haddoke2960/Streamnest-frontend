a30bfcc4f42e939492f00f585c2b337729af2609
haddoke2960 <haddoke@gmail.com>
2025-04-20 01:15:12 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { app, analytics } from './firebase';
+import AuthForm from "./AuthForm";
+
 export default function App() {
-  return (
-    <div className="h-screen w-full flex items-center justify-center bg-gradient-to-br from-indigo-600 to-blue-500 text-white text-3xl font-bold">
-      StreamNest is live.
-    </div>
-  )
+  return <AuthForm />;
 }
