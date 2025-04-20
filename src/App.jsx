629d1af68b6cef047a7d42646e3a32d7ce7bfbd4
haddoke2960 <haddoke@gmail.com>
2025-04-20 00:44:28 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { app, analytics } from './firebase';
 export default function App() {
   return (
     <div className="h-screen w-full flex items-center justify-center bg-gradient-to-br from-indigo-600 to-blue-500 text-white text-3xl font-bold">
