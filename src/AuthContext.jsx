cda3263026b684a1c36d8c0f602c5c142dfe6f66
haddoke2960 <haddoke@gmail.com>
2025-04-20 01:07:41 -0400

    Create AuthContext.jsx

---
+++ src/AuthContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { app } from './firebase';
+
+const AuthContext = createContext();
+const auth = getAuth(app);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
