0b1fe4dc87795c8a1d3c8c87cb849cac97302f4b
haddoke2960 <haddoke@gmail.com>
2025-04-20 00:38:23 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqez8nv1WvD8X...your_key...",
+  authDomain: "streamnest-5c9b6.firebaseapp.com",
+  projectId: "streamnest-5c9b6",
+  storageBucket: "streamnest-5c9b6.appspot.com",
+  messagingSenderId: "888388746012",
+  appId: "1:888388746012:web:5bb2f4282374d8e8",
+  measurementId: "G-8YPBW8F9LD"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
