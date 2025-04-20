413ee41bbe5b5b98bd19071b065e9d4e565a8c52
haddoke2960 <haddoke@gmail.com>
2025-04-20 01:30:42 -0400

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-gradient-to-br from-blue-600 to-indigo-600 text-white text-2xl font-semibold">
+      Welcome to StreamNest Home!
+    </div>
+  );
+}
