5825936f196adf8018183bcade30ce2e7480fc9e
haddoke2960 <haddoke@gmail.com>
2025-04-20 02:56:29 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App";
-import About from "./About";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AppRouter from "./Router";
+import { AuthProvider } from "./AuthContext"; // Import AuthProvider
 
-export default function AppRouter() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  </React.StrictMode>
+);
