07db3eb5461ea09e66fd809275b327d6739f1ba4
haddoke2960 <haddoke@gmail.com>
2025-04-21 01:42:25 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
-import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-const db = getFirestore(app);
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // <-- Add this line
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCqez8nv1WvD8X...your_key...",
+  apiKey: "AIzaSyCqez8nv1WvD8X...your_key_here",
   authDomain: "streamnest-5c9b6.firebaseapp.com",
   projectId: "streamnest-5c9b6",
   storageBucket: "streamnest-5c9b6.appspot.com",
   messagingSenderId: "888388746012",
-  appId: "1:888388746012:web:5bb2f4282374d8e8",
+  appId: "1:888388746012:web:5bb2f428237...",
   measurementId: "G-8YPBW8F9LD"
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app); // <-- Add this line
 
-export { app, analytics, db };
+export { app, analytics, db, auth }; // <-- Export auth
