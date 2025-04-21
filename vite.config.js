a95ecfd450b9d20d42487287f0d5d8e8446bc3b3
haddoke2960 <haddoke@gmail.com>
2025-04-20 22:24:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',     // allows access from other devices (iPhone, etc.)
-    port: 5173,          // same port you're using
-    strictPort: true     // fail fast if port is taken
-  },
-  build: {
-    outDir: 'dist'
+    host: '0.0.0.0',
+    port: 5173
   }
 });
