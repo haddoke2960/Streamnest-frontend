3a419b2c4a23ea34bb81651d300d2d841ce163c0
haddoke2960 <haddoke@gmail.com>
2025-04-21 23:51:50 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import AuthForm from './AuthForm';
-import Dashboard from './Dashboard';
-import PrivateRoute from './PrivateRoute';
-import Home from './Home'; // Add this if you have a home page
-import Navbar from './Navbar'; // Import Navbar component
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Navbar /> {/* Add Navbar at the top level */}
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<AuthForm isSignUp={false} />} />
-        <Route path="/signup" element={<AuthForm isSignUp={true} />} />
-
-        {/* Protected Routes */}
-        <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-
-        {/* Optional: 404 Page */}
-        <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Streamnest Auth</h1>
+      <AuthForm />
+    </div>
   );
-}
+};
 
 export default App;
