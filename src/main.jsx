2e6b2f2a26ebe8b57b70a9fce7e16860e6285e75
haddoke2960 <haddoke@gmail.com>
2025-04-22 00:11:36 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // ← NOTE: no extension
-
+import App from './App'; // <-- no extension = best practice in Vite
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
